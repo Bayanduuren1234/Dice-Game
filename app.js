ab2a2ef248e5c7e38b0ba1d425eec47ffbb42184
Bayanduuren <bayakabaynduuren@gmail.com>
2022-09-05 12:36:46 +0800

    Toglogchiin eeljiig solidog bolgono

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглох ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчыг 1 гэж тэмдэглэе
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
@@ -21,9 +21,48 @@ document.getElementById("current-1").textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 
+// Шоог шидэх эвент листенер
 document.querySelector(".btn-roll").addEventListener("click", function () {
   var diceNumber = Math.floor(Math.random() * 6) + 1;
 
   diceDom.style.display = "block";
   diceDom.src = "dice-" + diceNumber + ".png";
+
+  if (diceNumber !== 1) {
+    roundScore += diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    diceDom.style.display = "none";
+  }
+});
+
+// Hold button-ийг ашиглах эвент листенер
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  if (activePlayer === 0) {
+    document.getElementById("score-0").textContent = scores[0] += roundScore;
+    document.getElementById("current-0").textContent = 0;
+    roundScore = 0;
+    diceDom.style.display = "none";
+
+    activePlayer === 1 ? (activePlayer = 0) : (activePlayer = 1);
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+  } else {
+    document.getElementById("score-1").textContent = scores[1] += roundScore;
+    document.getElementById("current-1").textContent = 0;
+    roundScore = 0;
+    diceDom.style.display = "none";
+
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    document.getElementById("current-" + activePlayer);
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+  }
 });
